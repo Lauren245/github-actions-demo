c94155c68d2f7126d74b396efe43341247bc6ee9
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2025-03-13 19:26:10 -0700

    added comment to calculator.tsx to check the functionality of GitHub Actions.

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -4,7 +4,7 @@ interface CalculatorState {
   total: number;
   previousTotal: number;
 }
-
+//adding comment to trigger GitHub actions
 export default function Calculator() {
   const [calcState, setCalcState] = useState<CalculatorState>({ total: 0, previousTotal: 0 });
   const [value, setValue] = useState(0);
